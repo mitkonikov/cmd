9e9263410dae247f6f79a40c0fb2559f2b713510
Mitko Nikov <mitkonikov01@gmail.com>
2015-04-14 22:24:36 +0200

    Adding the project

---
+++ MConsole/MConsole/Main.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace MConsole
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.UpTimer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Black;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.textBox1.ForeColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(32, 9);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(514, 285);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            this.textBox1.MouseHover += new System.EventHandler(this.textBox1_MouseHover);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Location = new System.Drawing.Point(481, -37);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(101, 101);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseHover += new System.EventHandler(this.pictureBox1_MouseHover);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = ">";
+            // 
+            // UpTimer1
+            // 
+            this.UpTimer1.Tick += new System.EventHandler(this.UpTimer1_Tick);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(549, 297);
+            this.ControlBox = false;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Main";
+            this.Text = "MConsole Main";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer UpTimer1;
+    }
+}
+
---
+++ MConsole/MConsole/Main.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MConsole
+{
+    public partial class Main : Form
+    {
+        // MConsole V1
+        // IF YOU MAKE MODIFICATIONS PLEASE CREDIT MITKO NIKOV
+        // AND I WANT TO SEE YOUR MODIFICATIONS AND MAYBE WE WILL HAVE NEW VERSION
+        // SEND ME THOSE TO MY EMAIL : mitkonikov01@gmail.com
+
+        // Current Line
+        int Line = 0;
+
+        public Main()
+        {
+            InitializeComponent();
+            Dec1(); //Decoration Method
+            textBox1.Focus();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13) // if Enter is pressed
+            {
+                // exec the command
+                try
+                {
+                    if (textBox1.Lines[Line] == "" & Line == 0)
+                    {
+                        Console.WriteLine("Empty Line!"); // Just for signalling
+                    }
+                    else
+                    {
+                        // Line = 0 (for the first time)
+                        if (textBox1.Lines[Line] == "Help" || textBox1.Lines[Line] == "help" || textBox1.Lines[Line] == "HELP")
+                        {
+                            // Exec the help command
+                            LineLOC(); // Add a line for the Showing Text
+                            // Add a text into the textbox1
+                            textBox1.Text = textBox1.Text + Environment.NewLine + "CLOSE -- Closing the app!";
+                            LineLOC(); // Add a line for the next user input
+                            // ---- Set the cursor at the last letter ----
+                            textBox1.SelectionStart = textBox1.Text.Length;
+                            textBox1.SelectionLength = 0;
+                            // -------------------------------------------
+                        }
+                        else
+                        {
+                            if (textBox1.Lines[Line] == "close" || textBox1.Lines[Line] == "close")
+                            {
+                                //Exec the close command
+                                Close();
+                            }
+                            else
+                            {
+                                // here another command
+                                // WARNING: You must call LineLOC method in the last function (ex. close) in the else brackets
+                                LineLOC();
+                            }
+                        }
+                    }
+                }
+                catch
+                {
+                    // There will be error if you dont fill the first command line with any text
+                    // So I put here a "catch"
+                    LineLOC();
+                }
+            }
+        }
+
+        private void LineLOC() // Line Location Method
+        {
+            Line++;
+            if (Line < 17)
+            {
+                // Sets up the location of the prompt (">")
+                // You can change the label1 Text when you are in different folders
+                label1.Location = new Point(label1.Location.X, label1.Location.Y + 16);
+            }
+        }
+
+        private void textBox1_MouseHover(object sender, EventArgs e)
+        {
+            // THERE IS SOME TIME DELAY IN WHICH YOU CAN TRY TO CHANGE SOMETHING
+            // IF YOU KNOW HOW TO FIX IT OR YOU HAVE ALTERNATIVE WAY OF PREVENTING
+            // YOU SHOULD WRITE TO MY EMAIL : mitkonikov01@gmail.com
+            textBox1.Enabled = false; // To prevent changing the words
+            // ---- Set the cursor at the last letter ----
+            textBox1.SelectionStart = textBox1.Text.Length;
+            textBox1.SelectionLength = 0;
+            // -------------------------------------------
+            label1.Text = ""; //It hides the label text (instead of label1.Hide();)
+        }
+
+        private void pictureBox1_MouseHover(object sender, EventArgs e)
+        {
+            textBox1.Enabled = true;
+            textBox1.Focus();
+            label1.Text = ">"; //It revivals the label text again
+            // ---- Set the cursor at the last letter ----
+            textBox1.SelectionStart = textBox1.Text.Length;
+            textBox1.SelectionLength = 0;
+            // -------------------------------------------
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up) //Keys.Up is the Upper Arrow
+            {
+                UpTimer1.Start();
+            }
+        }
+
+        private void UpTimer1_Tick(object sender, EventArgs e)
+        {
+            // THERE MUST BE SOME DELAY BECAUSE IT WILL RUN THE "SET UP CURSOR"
+            // FIRST BEFORE THE UPPER KEY IS REGISTERED IN THE TEXTBOX
+            // ---- Set the cursor at the last letter ----
+            textBox1.SelectionStart = textBox1.Text.Length;
+            textBox1.SelectionLength = 0;
+            // -------------------------------------------
+            UpTimer1.Stop();
+        }
+
+        private void Dec1()
+        {
+            // JUST FOR DECORATION
+            Bitmap b1;
+            Graphics g1;
+            b1 = new Bitmap(102, 102);
+            g1 = Graphics.FromImage(b1);
+            g1.DrawEllipse(new Pen(Color.FromArgb(107, 173, 246), 2f), 0, 0, 100, 100);//-------------------
+            g1.DrawEllipse(new Pen(Color.FromArgb(107, 173, 246), 3f), 20, 20, 60, 60);//Draws some eclipses
+            g1.DrawEllipse(new Pen(Color.FromArgb(107, 173, 246), 4f), 40, 40, 20, 20);//-------------------
+            pictureBox1.Image = b1;
+            g1.Dispose();
+        }
+
+    }
+}
